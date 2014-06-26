ca9b09d97bda8fcb714912cb5b3faf7546801d6c
Lindsay-R <lrecknell@gmail.com>
2014-06-26 09:43:01 -0600

    Done

--- lib/dna_strand_pair.rb
+++ lib/dna_strand_pair.rb
@@ -1,3 +1,22 @@
 class DnaStrandPair
 
+  def initialize(strand_1, strand_2)
+    @strand_1 = strand_1
+    @strand_2 =strand_2
+
+  end
+
+
+  def hamming_distance
+    index = 0
+    ham = 0
+    while index < [(@strand_1.length),(@strand_2.length)].min
+      if @strand_1[index] != @strand_2[index] then ham += 1
+      end
+      index +=1
+    end
+    return ham
+  end
+
+
 end
